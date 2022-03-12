3a915ba35b71cc5dc70d236071dccb1596617984
fredydlemus <fredyflemus@gmail.com>
2022-03-12 00:39:05 -0600

    configure notifications

--- src/pages/Timer.js
+++ src/pages/Timer.js
@@ -5,7 +5,24 @@ const Timer = () => {
     const [timer, setTimer] = React.useState(3);
     const [timeLeft, setTimeLeft] = React.useState(0);
 
-    const start = () => {
+    const start = async() => {
+
+        if(! ('Notification' in window) || !('serviceWorker' in navigator)) {
+            return alert('Your browser does not support notifications');
+        }
+
+        if(Notification.permission === 'default') {
+            await Notification.requestPermission();
+        }
+
+        if(Notification.permission === 'blocked') {
+            return alert('You have blocked notifications');
+        }
+
+        if(Notification.permission !== 'granted') {
+            return;
+        }
+
         let timerStart = timer;
         setTimeLeft(timerStart);
 
@@ -14,9 +31,20 @@ const Timer = () => {
             if (timerStart === 0) {
                 clearInterval(countDownInterval);
                 setTimeLeft(0);
+                showNotification();
             }
 
         }, 1000);
+    }
+
+    const showNotification = async() => {
+        const registration = await navigator.serviceWorker.getRegistration();
+        if(!registration) return alert('No service worker found');
+
+        registration.showNotification('Timer', {
+            body: 'Time is up',
+            icon: 'https://cdn.iconscout.com/icon/free/png-256/timer-clock-time-clock-alarm-alarm-clock-clock-time-clock-timer-56989.png',
+        })
     }
 
 
